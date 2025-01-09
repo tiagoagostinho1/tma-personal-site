8b09e79a523e70519ec5121f24e61df89af0c0a7
tmagostinho <tiagomiguelagostinho@gmail.com>
2025-01-09 17:12:09 +0000

    add new images and improve the form

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,12 +1,16 @@
+import { Button } from './Button'
+
 export function ContactForm() {
+  const inputsClasses =
+    'block w-full rounded-md bg-white dark:bg-neutral-800 px-3.5 py-2 text-base text-gray-900 dark:text-neutral-300 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2'
+  const labelClasses =
+    'block text-sm/6 font-semibold text-gray-900 dark:text-zinc-100'
+
   return (
     <form action="#" method="POST" className="mx-auto mt-16 max-w-xl sm:mt-20">
       <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
         <div className="sm:col-span-2">
-          <label
-            htmlFor="name"
-            className="block text-sm/6 font-semibold text-gray-900"
-          >
+          <label htmlFor="name" className={labelClasses}>
             Name
           </label>
           <div className="mt-2.5">
@@ -15,16 +19,13 @@ export function ContactForm() {
               name="name"
               type="text"
               autoComplete="given-name"
-              className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+              className={inputsClasses}
             />
           </div>
         </div>
 
         <div className="sm:col-span-2">
-          <label
-            htmlFor="email"
-            className="block text-sm/6 font-semibold text-gray-900"
-          >
+          <label htmlFor="email" className={labelClasses}>
             Email
           </label>
           <div className="mt-2.5">
@@ -33,15 +34,12 @@ export function ContactForm() {
               name="email"
               type="email"
               autoComplete="email"
-              className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+              className={inputsClasses}
             />
           </div>
         </div>
         <div className="sm:col-span-2">
-          <label
-            htmlFor="message"
-            className="block text-sm/6 font-semibold text-gray-900"
-          >
+          <label htmlFor="message" className={labelClasses}>
             Message
           </label>
           <div className="mt-2.5">
@@ -49,19 +47,16 @@ export function ContactForm() {
               id="message"
               name="message"
               rows={4}
-              className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+              className={inputsClasses}
               defaultValue={''}
             />
           </div>
         </div>
       </div>
       <div className="mt-10">
-        <button
-          type="submit"
-          className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-        >
+        <Button type="submit" className="w-full">
           Let's talk
-        </button>
+        </Button>
       </div>
     </form>
   )
