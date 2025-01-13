4f11a9cabd0f919c1e2a4bbac5c212e8e6802470
tmagostinho <tiagomiguelagostinho@gmail.com>
2025-01-13 10:28:53 +0000

    fixed menu mobile version, updated git ignore and text improvements

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -106,13 +106,6 @@ export default function About() {
               >
                 Follow on LinkedIn
               </SocialLink>
-              <SocialLink
-                href="mailto:tiagomiguelagostinho@gmail.com"
-                icon={MailIcon}
-                className="mt-8 border-t border-zinc-100 pt-8 dark:border-zinc-700/40"
-              >
-                tiagomiguelagostinho@gmail.com
-              </SocialLink>
             </ul>
           </div>
         </div>
